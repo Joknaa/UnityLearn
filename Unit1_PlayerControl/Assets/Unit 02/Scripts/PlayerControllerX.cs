0777729cf52a063e62056e72a9450506f0dd88ed
Oknaa2.0 <mohammad.laadidaoui.acrab@gmail.com>
2023-08-02 17:54:51 +0100

    Unit 02 Lesson 2.1

---
+++ Unit1_PlayerControl/Assets/Unit 02/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour {
+    public float speed;
+    public float xRange;
+    public float horizontalInput;
+
+    private void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * (horizontalInput * speed * Time.deltaTime));
+        
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange) {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+    }
+}
